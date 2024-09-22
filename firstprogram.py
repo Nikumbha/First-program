e6032128ac0e0a73c15e1ffdee0cedcc7900e7f7
Nikumbha <50376915+Nikumbha@users.noreply.github.com>
2024-09-22 19:16:40 +0530

    Update firstprogram.py

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,12 @@
-print(2026)
+num1 = int(input("enter 1num"))
+num2 = int(input("enter 2num"))
+num3 = int(input("enter 3num"))
+
+if num1 == num2 and num2 == num3:
+  print("All nums are equal")
+elif num1 > num2 and num1 > num3:
+  print("num1 is greater")
+elif num2 > num1 and num2 > num3:
+  print("num2 is greater")
+elif num3 > num1 and num3 > num2:
+  print("num3 is greater")
